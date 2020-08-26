e7b0ae65d98d9c9c1a28498c859ec29a376470ac
Baptiste Seux <baptiste.seux@grenoble-inp.org>
2020-08-26 10:31:13 +0200

    bluetooth embbeded test file

--- embedded/module/src/main.cpp
+++ embedded/module/src/main.cpp
@@ -9,6 +9,9 @@
 #include <BLE2902.h>
 
 #include <Arduino.h>
+
+#define MODULE_NAME "MODULE_123"
+#define PRIVATE_ID "456"
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
@@ -19,6 +22,7 @@ BLEServer *pServer = NULL;
 
 String ssid = "";
 String pwd = "";
+String privateId = "";
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 std::string value = "0";
@@ -51,21 +55,44 @@ class MyCallbacks : public BLECharacteristicCallbacks
                     {
                         ssid = String(ssid + value[i]);
                     }
-                    else
+                    else if (n == 1)
                     {
                         pwd = String(pwd + value[i]);
                     }
+                    else
+                    {
+                        privateId = String(privateId + value[i]);
+                    }
                 }
             }
-            if (ssid == "toto" && pwd == "tata")
+            delay(3000);
+            Serial.print("privateId: ");
+            Serial.println(privateId);
+            Serial.print("PRIVATE_ID: ");
+            Serial.println(PRIVATE_ID);
+            if (privateId == PRIVATE_ID)
             {
-                valToNotify = 1;
-                // pCharacteristic->setValue("1");
+                if (ssid == "toto" && pwd == "tata")
+                {
+                    // valToNotify = 1;
+                    // pCharacteristic->setValue("1");
+                    pCharacteristic->setValue("0");
+                    Serial.printf("val to notify: 0");
+                }
+                else
+                {
+                    // valToNotify = 0;
+                    // pCharacteristic->setValue("0");
+                    pCharacteristic->setValue("1");
+                    Serial.printf("val to notify: 1");
+                }
             }
             else
             {
-                valToNotify = 0;
+                // valToNotify = 0;
                 // pCharacteristic->setValue("0");
+                pCharacteristic->setValue("2");
+                Serial.printf("val to notify: 2");
             }
 
             Serial.println();
@@ -73,7 +100,7 @@ class MyCallbacks : public BLECharacteristicCallbacks
             Serial.printf("ssid: %s et pwd: %s\n", ssid, pwd);
             ssid = "";
             pwd = "";
-            Serial.printf("val to notify: %d", valToNotify);
+            privateId = "";
         }
     }
 };
@@ -105,16 +132,16 @@ void setup()
     pCharacteristic = pService->createCharacteristic(
         CHARACTERISTIC_UUID,
         BLECharacteristic::PROPERTY_READ |
-        BLECharacteristic::PROPERTY_WRITE |
-        BLECharacteristic::PROPERTY_READ |
-        BLECharacteristic::PROPERTY_NOTIFY);
+            BLECharacteristic::PROPERTY_WRITE |
+            BLECharacteristic::PROPERTY_READ |
+            BLECharacteristic::PROPERTY_NOTIFY);
     // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
     // Create a BLE Descriptor
     pCharacteristic->addDescriptor(new BLE2902());
 
     pCharacteristic->setCallbacks(new MyCallbacks());
 
-    pCharacteristic->setValue(valToNotify);
+    pCharacteristic->setValue((uint8_t *)&valToNotify, 4);
     pService->start();
 
     BLEAdvertising *pAdvertising = pServer->getAdvertising();
@@ -124,20 +151,20 @@ void setup()
 
 void loop()
 {
-    if (deviceConnected)
-    {
-        Serial.print("[");
-        for (int i = 0; i < valToNotify.length(); i++)
-        {
-            Serial.printf("%d,", valToNotify[i]);
-        }
-        Serial.print("]");
-        pCharacteristic->setValue((uint8_t *)&valToNotify, 4);
-        pCharacteristic->notify();
-        // Serial.printf("val to notify: %d", valToNotify);
-        delay(600); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
-        // }
-    }
+    // if (deviceConnected)
+    // {
+    //     // Serial.print("[");
+    //     // for (int i = 0; i < valToNotify.length(); i++)
+    //     // {
+    //     Serial.printf("%d", valToNotify);
+    //     // }
+    //     Serial.print("]");
+    //     pCharacteristic->setValue((uint8_t *)&valToNotify, 4);
+    //     pCharacteristic->notify();
+    //     // Serial.printf("val to notify: %d", valToNotify);
+    //     delay(2000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+    //     // }
+    // }
     // disconnecting
     if (!deviceConnected && oldDeviceConnected)
     {
